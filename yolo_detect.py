d72b90b56f4773bc067bf1ccfe68b8f413fbe87c
yuminglong318 <yuminglong318@gmail.com>
2023-10-17 22:32:29 +0800

    refactor

--- yolo_detect.py
+++ yolo_detect.py
@@ -2,95 +2,115 @@ import cv2
 import numpy as np
 import os
 
-# Files
-cfg_file = 'models/cfg/yolov3-tiny-feet.cfg'                            # Your .cfg file
-weights_file = 'models/weights/yolov3-tiny-feet_final.weights'               # Your .weights file
-classes = ['feet']                                           # Your classes file
-image_directory = './feet_data/train'                        # Input image
 
-# with open(classes_file, 'r') as f:
-#     classes = f.read().split('\n')
+def load_net(cfg_file= 'models/cfg/yolov3-tiny-feet.cfg', weights_file= 'models/weights/yolov3-tiny-feet_final.weights'):
 
-# Load the network
-net = cv2.dnn.readNetFromDarknet(cfg_file, weights_file)
+    net = cv2.dnn.readNetFromDarknet(cfg_file, weights_file)
 
-for image_name in os.listdir(image_directory):
+    return net
 
-    if image_name.endswith('.jpg'):
-        image_file = os.path.join(image_directory, image_name)
+def detect_feet(image_file, net):
 
-        # Load the image
-        image = cv2.imread(image_file)
+    # Load the image
+    image = cv2.imread(image_file)
 
-        height, width, _ = image.shape
+    # height and width of image
+    height, width, _ = image.shape
 
-        # Get the output layer names
-        layer_names = net.getLayerNames()
-        output_layers = net.getUnconnectedOutLayersNames()
+    layer_names = net.getLayerNames()
+    output_layers = net.getUnconnectedOutLayersNames()
 
-        # Prepare the image for inference
-        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (640, 640), (0,0,0), True, crop=False)
+    # Prepare the image for inference
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (640, 640), (0,0,0), True, crop=False)
 
-        # Set the new input value for the network
-        net.setInput(blob)
+    # Set the new input value for the network
+    net.setInput(blob)
 
-        # Run a forward pass
-        outs = net.forward(output_layers)
+    # Run a forward pass
+    outs = net.forward(output_layers)
 
-        count = 0
-        feet = []
+    # detected feet list
+    feet = []
 
-        for out in outs: 
-            for detection in out: 
-                # Each detection has the x, y, w and h normalized (from 0 to 1) relative to the width and the height of the image
-                scores = detection[5:] 
-                class_id = np.argmax(scores) 
-                confidence = scores[class_id]
+    for out in outs: 
+        for detection in out: 
+            # Each detection has the x, y, w and h normalized (from 0 to 1) relative to the width and the height of the image
+            scores = detection[5:] 
+            class_id = np.argmax(scores) 
+            confidence = scores[class_id]
 
-                if confidence > 0.2:
+            if confidence > 0.2:
 
-                    center_x = int(detection[0] * width)
-                    center_y = int(detection[1] * height)
-                    w = int(detection[2] * width)
-                    h = int(detection[3] * height)
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
 
-                    feet.append({'confidence': confidence, 'cx': center_x, 'cy': center_y, 'width': w, 'height': h})
+                feet.append({'confidence': confidence, 'cx': center_x, 'cy': center_y, 'width': w, 'height': h})
+
+    return feet
+
+def draw_bounding_box(image_file, feet):
+
+    # Load the image
+    image = cv2.imread(image_file)
+
+    # height and width of image
+    height, width, _ = image.shape
+
+    if(len(feet) == 0):
+        return image
+
+    else:
+
+        center_x = sum(f['cx'] for f in feet) / len(feet)
+        center_y = sum(f['cy'] for f in feet) / len(feet)
+        w = sum(f['width'] for f in feet) / len(feet)
+        h = sum(f['height'] for f in feet) / len(feet)
+
+        # foot = max(feet, key = lambda e: e['confidence'])
+        # center_x = foot['cx']
+        # center_y = foot['cy']
+        # w = foot['width']
+        # h = foot['height']
+        
+        # Rectangle coordinates
+        x = int(center_x - w / 2)
+        y = int(center_y - h / 2)
+
+        # Ensure the bounding boxes are within the bounds of the image
+        x = max(min(x, width - 1), 0)
+        y = max(min(y, height - 1), 0)
+        w = int(min(width - x - 1, w))
+        h = int(min(height - y - 1, h))
+
+        # Draw the bounding box on the image
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        return image
 
 
-        if(len(feet) == 0):
-            print(image_name)
+if __name__ == '__main__':
 
-        else:
+    # cfg_file = 'models/cfg/yolov3-tiny-feet.cfg'                            # Your .cfg file
+    # weights_file = 'models/weights/yolov3-tiny-feet_final.weights'          # Your .weights file
+    
+    net = load_net()
 
-            center_x = sum(f['cx'] for f in feet) / len(feet)
-            center_y = sum(f['cy'] for f in feet) / len(feet)
-            w = sum(f['width'] for f in feet) / len(feet)
-            h = sum(f['height'] for f in feet) / len(feet)
+    image_directory = './feet_data/valid'                        # Input image
 
-            # foot = max(feet, key = lambda e: e['confidence'])
-            # center_x = foot['cx']
-            # center_y = foot['cy']
-            # w = foot['width']
-            # h = foot['height']
-            
-            # Rectangle coordinates
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
+    for image_name in os.listdir(image_directory):
 
-            # Ensure the bounding boxes are within the bounds of the image
-            x = max(min(x, width - 1), 0)
-            y = max(min(y, height - 1), 0)
-            w = int(min(width - x - 1, w))
-            h = int(min(height - y - 1, h))
+        if image_name.endswith('.jpg'):
+            image_file = os.path.join(image_directory, image_name)
 
-            # Draw the bounding box on the image
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(image, classes[class_id], (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            detections = detect_feet(image_file, net)
+
+            image = draw_bounding_box(image_file, detections)
+
+            cv2.imwrite(f"./results/{image_name}", image)
 
 
-        # # Display the image
-        # cv2.imshow("Image", image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
-        cv2.imwrite(f"./results/{image_name}", image)
+
+
